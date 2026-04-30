53b4b88305a97f73b4731f752f8c241bc986cf41
Hein Htet Soe <heinhtet.25@ichat.sp.edu.sg>
2026-04-30 14:42:45 +0800

    added functions

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,40 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+      
+
+def get_user_input():
+    entered_input = input("Enter your numbers: ")
+    string_list = entered_input.split(",")
+    float_list = []
+    for num in string_list:
+        ndata_type = float(num)
+        float_list.append(ndata_type)
+    return float_list
+
+
+
+
+def calc_average_temperature(temperature_list):
+    total = sum(temperature_list)
+    average = total / len(temperature_list)
+    return float(average)
+
+def calc_min_max_temperature(list_temp):
+    min_temp = min(list_temp)
+    max_temp = max(list_temp)
+    int_min = int(min_temp)
+    int_max = int(max_temp)
+    final_list = [int_min , int_max]
+    return final_list
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+if __name__ == "__main__":
+    main()
+
+
+
